fa2adce7251cbf0623c3a9482b27f551f21698c7
code <2388224167@qq.com>
2024-12-23 07:37:31 +0200

    feat(eslint、prettier配置):

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 制表符宽度（用于可读性）
+    tabWidth: 2,
+    // 使用制表符进行缩进
+    useTabs: true,
+    // 在多行数组和对象的最后一项后添加尾逗号
+    trailingComma: "all",
+    // 不使用分号
+    semi: false,
+    // 使用双引号
+    singleQuote: false
+}
