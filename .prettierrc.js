6dd010d4bdf029482a1cdfef6d7c82590e07c972
code <2388224167@qq.com>
2024-12-23 13:56:13 +0200

    feat:nest-monorepo打包

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-    // 制表符宽度（用于可读性）
-    tabWidth: 2,
-    // 使用制表符进行缩进
-    useTabs: true,
-    // 在多行数组和对象的最后一项后添加尾逗号
-    trailingComma: "all",
-    // 不使用分号
-    semi: false,
-    // 使用双引号
-    singleQuote: false
+	// 制表符宽度（用于可读性）
+	tabWidth: 2,
+	// 使用制表符进行缩进
+	useTabs: true,
+	// 在多行数组和对象的最后一项后添加尾逗号
+	trailingComma: "all",
+	// 不使用分号
+	semi: false,
+	// 使用双引号
+	singleQuote: false,
+	// 一行最大长度
+	printWidth: 120,
 }
