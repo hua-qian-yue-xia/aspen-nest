ebadc0937c4d97a62eccccd7b3a77936d1af7afd
dzc <2388224167@qq.com>
2025-07-12 18:58:47 +0800

    fix(sys-role-service): 删除角色前检查是否存在
    在删除角色前先查询角色是否存在，避免无效删除操作

--- apps/admin/src/module/sys/service/sys-role-service.ts
+++ apps/admin/src/module/sys/service/sys-role-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
-import { Repository } from "typeorm"
+import { In, Repository } from "typeorm"
 import { plainToInstance } from "class-transformer"
 
 import { OrmQuery, Exception, cache, RedisTool } from "@aspen/aspen-core"
@@ -64,6 +64,9 @@ export class SysRoleService {
 
 	// 删除
 	async delByIds(roleIds: Array<number>): Promise<number> {
+		// 查询存不存在
+		const roleList = await this.sysRoleRep.findBy({ roleId: In(roleIds) })
+		if (!roleList.length) return 0
 		// 删除数据
 		const { affected } = await this.sysRoleRep.delete(roleIds)
 		// 删除缓存
